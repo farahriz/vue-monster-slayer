178b0c0a18aeac8a335596db9a4c1341a95d538a
farahriz <41633573+farahriz@users.noreply.github.com>
2019-05-17 07:15:46 +0800

    Add name prompt

--- src/App.vue
+++ src/App.vue
@@ -14,25 +14,35 @@
           <p>{{monsterHealth}}</p>
         </div>
       </div>
+      <div class="row">
+        <div id="controls" class="col-lg-12" v-if="gameRunning">
+          <b-button size="lg" variant="primary" v-on:click.stop.prevent="attack">ATK</b-button>
+          <b-button size="lg" variant="info" @click="specialAttack">Sp. ATK</b-button>
+          <b-button size="lg" variant="success" @click="heal">HEAL</b-button>
+          <b-button size="lg" variant="warning">RUN</b-button>
+        </div>
+
+        <div id="start-prompt" class="col-lg-12" v-else>
+          <template v-if="!playerName">
+            <p>What is your name, brave adventurer?</p>
+            <form class="form-inline justify-content-center">
+              <b-form-input v-model="playerName" placeholder="Enter your name"></b-form-input>
+              <b-button>ENTER</b-button>
+            </form>
+          </template>
+          <template v-else>
+            <span>Hello, {{playerName}}</span>
+            <span>Are you ready to begin?</span>
+            <b-button size="lg" variant="danger">LET'S GO</b-button>
+          </template>
+        </div>
+      </div>
+      <div class="row">
+        <div id="action-log" class="col-lg-12 bg-dark text-light">
+          <p>All the damage logs go here</p>
+        </div>
+      </div>
     </div>
-
-    <div id="controls" class="container">
-      <b-button size="lg" variant="primary" v-on:click.stop.prevent="attack" >ATK</b-button>
-      <b-button size="lg" variant="info" @click="specialAttack">Sp. ATK</b-button>
-      <b-button size="lg" variant="success" @click="heal" >HEAL</b-button>
-      <b-button size="lg" variant="warning">RUN</b-button>
-    </div>
-
-    <div id="start-prompt">
-      <span>Hello, {{playerName}}</span>
-      <span>Are you ready to begin?</span>
-      <b-button size="lg" variant="danger">LET'S GO</b-button>
-    </div>
-
-    <div id="action-log" class="container">
-
-    </div>
-
   </div>
 </template>
 
@@ -54,24 +64,24 @@ export default {
       playerHealth: 100,
       gameRunning: false,
       playerDeath: false,
-      playerName: "Tomas"
-    }
+      playerName: ""
+    };
   },
   methods: {
     attack: function() {
-      this.playerHealth -= this.calcDamage(7, 20)
-      this.monsterHealth -= this.calcDamage(3, 10)
+      this.playerHealth -= this.calcDamage(7, 20);
+      this.monsterHealth -= this.calcDamage(3, 10);
     },
     specialAttack: function() {
-      this.playerHealth -= this.calcDamage(7, 20)
-      this.monsterHealth -= this.calcDamage(15, 35)
+      this.playerHealth -= this.calcDamage(7, 20);
+      this.monsterHealth -= this.calcDamage(15, 35);
     },
     heal: function() {
-      this.playerHealth -= this.calcDamage(7, 15)
-      this.playerHealth += this.calcDamage(10, 30)
+      this.playerHealth -= this.calcDamage(7, 15);
+      this.playerHealth += this.calcDamage(10, 30);
     },
     calcDamage: function(min, max) {
-      return Math.max(Math.floor(Math.random()*max), min)
+      return Math.max(Math.floor(Math.random() * max), min);
     }
   }
 };
